02f35265da3a154e9d985a1f0eebe8e955f6f1a5
Vinibiiel <76178999+Vinibiiel@users.noreply.github.com>
2022-03-29 12:53:57 +0000

    Finalizei o mario-more, e iniciei o credit

---
+++ mario-more/mario.c
@@ -0,0 +1,43 @@
+/*
+  WEEK 1
+
+  this version of Mario if feeling more comfortable.
+  CODE MADE BY: Vinicius Gabriel
+  Github: https://github.com/Vinibiiel
+*/
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+  int hashes,aux;
+  printf("Height: ");
+  scanf("%d",&hashes);
+  // While verificador se o valor de hashes é aceitavel
+  while(hashes <=0 || hashes > 8){
+    printf("Height: ");
+    scanf(""); // Limpeza de buffer, o \n do enter
+    scanf("%d",&hashes);
+  }
+  // Variavel auxiliar usada a controlar o numero de linhas no for
+  aux = hashes;
+  for (int i = 1;i<=aux;i++){
+      for (int j = hashes;j>1;j--){
+          printf(" ");
+      }
+    hashes--;
+    for(int k = hashes-i;k<hashes;k++){
+      printf("#");
+    }
+
+    // Agora a parte do "espelho" dos #
+    printf(" ");
+    for(int k = hashes-i;k<hashes;k++){
+      printf("#");
+    }
+    for (int j = hashes;j>1;j--){
+          printf(" ");
+      }
+    printf("\n");
+  }
+}
